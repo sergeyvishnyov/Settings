eade43be46961330ecf2948a8a7c01dd6e24fa23
Sergey Vishnyov <sergey.vishnyov@gmail.com>
2022-01-20 13:22:03 +0300

    Animate View with 4 buttons

--- Settings/Extensions.swift
+++ Settings/Extensions.swift
@@ -15,3 +15,30 @@ extension UIViewController {
         return instantiateFromNib()
     }
 }
+
+extension UIImage {
+    func imageWithColor(_ color: UIColor) -> UIImage { // For iOS <= 12
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        color.setFill()
+
+        let context = UIGraphicsGetCurrentContext()
+        context?.translateBy(x: 0, y: size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        context?.setBlendMode(CGBlendMode.normal)
+
+        let rect = CGRect(origin: .zero, size: CGSize(width: size.width, height: size.height))
+        context?.clip(to: rect, mask: cgImage!)
+        context?.fill(rect)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
+    }
+}
+
+extension UIButton {
+    func setTintColor(_ color: UIColor) { // For iOS <= 12
+        setImage(imageView?.image?.imageWithColor(color), for: .normal)
+    }
+}
--- Settings/ViewControllers/SettingsViewController.swift
+++ Settings/ViewControllers/SettingsViewController.swift
@@ -7,13 +7,71 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+public enum ButtonType: Int {
+    case typeAirmode = 1
+    case typeCellular = 2
+    case typeWiFi = 3
+    case typeBluetooth = 4
 }
 
+class SettingsViewController: UIViewController, UIGestureRecognizerDelegate {
+    @IBOutlet private var view4: UIView!
+    
+    var animator = UIViewPropertyAnimator()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        longPressRecognizer.minimumPressDuration = 0.2
+        longPressRecognizer.delaysTouchesBegan = true
+        longPressRecognizer.delegate = self
+        view4.addGestureRecognizer(longPressRecognizer)
+
+    }
+    
+    // MARK: - Actions
+    @objc func handleLongPress(gestureReconizer: UILongPressGestureRecognizer) {
+        if gestureReconizer.state == .began {
+            print("longpressed began")
+            animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeOut){
+                self.view4.transform = CGAffineTransform.identity.scaledBy(x: 0.9, y: 0.9)
+            }
+            animator.addCompletion { position in
+                print("animator.addCompletion")
+            }
+            animator.startAnimation()
+        }
+        if gestureReconizer.state == .ended {
+            animator.stopAnimation(true)
+            UIView.animate(withDuration: 0.25) {
+                self.view4.transform = CGAffineTransform.identity
+            }
+            print("longpressed ended")
+            
+//            let generator = UIImpactFeedbackGenerator(style: .light)
+//            generator.impactOccurred()
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        switch sender.tag {
+        case ButtonType.typeAirmode.rawValue:
+            sender.backgroundColor = sender.isSelected ? .orange : .gray
+        case ButtonType.typeCellular.rawValue:
+            sender.backgroundColor = sender.isSelected ? .systemGreen : .gray
+        case ButtonType.typeWiFi.rawValue:
+            sender.backgroundColor = sender.isSelected ? .systemBlue : .white
+            sender.setTintColor(sender.isSelected ? .white : .gray)
+        case ButtonType.typeBluetooth.rawValue:
+            sender.backgroundColor = sender.isSelected ? .systemBlue : .white
+            sender.setTintColor(sender.isSelected ? .white : .gray)
+        default:
+            break
+        }
+        print("bluetoothButtonPressed")
+    }
+    
+    
+}
