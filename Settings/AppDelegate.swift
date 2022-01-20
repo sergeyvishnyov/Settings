b75192a72e36ac3fe7c3795d341a56503ba570be
Sergey Vishnyov <sergey.vishnyov@gmail.com>
2022-01-20 10:57:06 +0300

    Create Project

---
+++ Settings/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Settings
+//
+//  Created by Sergey Vishnyov on 20.01.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
